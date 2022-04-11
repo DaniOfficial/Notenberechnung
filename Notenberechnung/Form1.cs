1e4ff2431a031bc8a810a3c3367823095e6222fa
Winke <daniel.winke@uni-passau.de>
2022-04-11 10:36:20 +0200

    Notenberechnung mit 4 Noten der SA im ersten Fach & Überprüfung auf Boxinhalt

--- Notenberechnung/Form1.cs
+++ Notenberechnung/Form1.cs
@@ -26,13 +26,56 @@ namespace Notenberechnung
         {
             //Button Click Programm
 
+            if(SA1_1.Text == "")
+            {
+                SA1_1.Text = "0";
+            }
+            else
+            {
+
+            }
+            if (SA1_2.Text == "")
+            {
+                SA1_2.Text = "0";
+            }
+            else
+            {
+
+            }
+
             //Fach1
             Fach1_Text.Text = Fach1.Text;
-            //Text zu INT mit Berechnung
+            
+            //Überprüfung auf Notenanzahl
+            int i = 0;
+            if (Convert.ToInt32(SA1_1.Text) > 0)
+            {
+                i++;
+            }
+            if(Convert.ToInt32(SA1_2.Text) > 0)
+            {
+                i++;
 
-            int Fach1_Note_Berechnung = Convert.ToInt32(SA1_1.Text) + Convert.ToInt32(SA1_2.Text);
+            }
+            if (Convert.ToInt32(SA1_3.Text) > 0)
+            {
+                i++;
+
+            }
+            if (Convert.ToInt32(SA1_4.Text) > 0)
+            {
+                i++;
+
+                
+                
+
+            }
+            int Fach1_Note_Berechnung = (Convert.ToInt32(SA1_1.Text) + Convert.ToInt32(SA1_2.Text) + Convert.ToInt32(SA1_3.Text) + Convert.ToInt32(SA1_4.Text)) / i;
             //Ausgabe mit Convertierung INT zu String
             Fach1_Note.Text = Convert.ToString(Fach1_Note_Berechnung);
+
+
+
 
 
         }
